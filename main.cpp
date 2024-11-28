56daa7f9ffb9aca86299088d9cbb3ad28ff517d7
Tyler <serpbtw@gmail.com>
2024-11-28 12:47:35 -0500

    Created dataset implementation as nodes in the main file, adjusted car.h and .cpp as needed.

--- main.cpp
+++ main.cpp
@@ -12,16 +12,16 @@ int main(){
     // List for testing purposes;
     // 3 2 5 0 1 8 7 6 9 4
     vector<Car*> Testlist;
-    Testlist.emplace_back(new Car("make", "model", 2024, 3, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 2, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 5, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 0, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 1, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 8, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 7, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 6, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 9, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
-    Testlist.emplace_back(new Car("make", "model", 2024, 4, 10, 10, "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 3, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 2, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 5, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 0, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 1, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 8, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 7, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 6, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 9, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
+    Testlist.emplace_back(new Car("make", "model", 2024, 4, 10, "10", "engine", "transmission", "drivetrain", "fuelType", "sellerName", "sellerRating", 0, 0, 0));
 
     Quicksort(Testlist, "price");
 
