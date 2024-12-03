3ea2f783287773028e2f9205b7ddbfff03d7e9d3
Alan Pham <alanpham@ufl.edu>
2024-12-03 17:28:37 -0500

    Created heapsort header file and cpp

---
+++ HeapSort.cpp
@@ -0,0 +1,36 @@
+//
+// Created by phama on 12/3/2024.
+//
+#include "HeapSort.h"
+#include "GetVal.h"
+
+void Heapify(vector<Car*>& list, int n, int i, string& spec) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && getVal(list[left], spec) > getVal(list[largest], spec)) {
+        largest = left;
+    }
+
+    if (right < n && getVal(list[right], spec) > getVal(list[largest], spec)) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(list[i], list[largest]);
+        Heapify(list, n, largest, spec);
+    }
+}
+
+void HeapSort(vector<Car*>& list, string spec) {
+    int n = list.size();
+
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        Heapify(list, n, i, spec);
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        swap(list[0], list[i]);
+        Heapify(list, i, 0, spec);
+    }
+}
